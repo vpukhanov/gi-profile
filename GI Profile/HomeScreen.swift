20ac900529060883c56c4d6d321f402e20c60b34
Vyacheslav Pukhanov <vyacheslav@hey.com>
2022-10-13 20:34:27 +0300

    add characters and namecards resources

---
+++ GI Profile/HomeScreen.swift
@@ -0,0 +1,25 @@
+//
+//  HomeScreen.swift
+//  GI Profile
+//
+//  Created by Vyacheslav Pukhanov on 13/10/2022.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        NavigationView {
+            List {
+                
+            }
+            .navigationTitle("Profiles")
+        }
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
