fecd0d0d4c07dab0ddecf33b216b659e019e4779
Vyacheslav Pukhanov <vyacheslav@hey.com>
2022-10-13 20:12:51 +0300

    initial commit

---
+++ GI Profile/GIProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  GI_ProfileApp.swift
+//  GI Profile
+//
+//  Created by Vyacheslav Pukhanov on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct GIProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
